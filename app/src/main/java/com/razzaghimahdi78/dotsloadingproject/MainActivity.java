756b1dbcbaab617d769b80ef556050416ea90783
razaghimahdi <61207818+razaghimahdi@users.noreply.github.com>
2020-10-18 11:45:36 +0330

    first commit

---
+++ app/src/main/java/com/razzaghimahdi78/dotsloadingproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.razzaghimahdi78.dotsloadingproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
