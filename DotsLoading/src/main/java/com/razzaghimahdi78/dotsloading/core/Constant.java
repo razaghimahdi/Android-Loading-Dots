14e875de39693318bdb4d834c2b5a9aabdf6e679
razaghimahdi <razaghimahdi78@gmail.com>
2023-01-24 16:17:28 +0330

    - Added Constant.java: to declare some constant values

---
+++ DotsLoading/src/main/java/com/razzaghimahdi78/dotsloading/core/Constant.java
@@ -0,0 +1,21 @@
+package com.razzaghimahdi78.dotsloading.core;
+
+import android.graphics.Color;
+
+public class Constant {
+
+    public static final float SIN_45 = 0.7071f;
+
+    public static final int DEFAULT_DOTS_SIZE = 20;
+
+    public static final int DEFAULT_DOTS_COUNT = 3;
+
+    public static final int DEFAULT_CIRCLE_DOTS_COUNT = 8;
+
+    public static final int DEFAULT_DURATION = 350;
+
+    public static final int DEFAULT_COLOR = Color.parseColor("#FF3700B3");
+
+    public static final int MAX_JUMP = 6;
+
+}
